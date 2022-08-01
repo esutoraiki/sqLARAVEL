ea2c012d4fd38fc0192b2b719a29875caeef15af
John Sánchez Alvarez <esutoraiki@gmail.com>
2022-08-01 10:01:57 -0500

    Correction paths for install

--- install.js
+++ install.js
@@ -2,12 +2,23 @@ const
     fs = require("fs"),
 
     // args = process.argv.slice(2),
-    dest = "../../",
-    filesDir = "files/"
+    dest_r = "./",
+    dest_p = "../public",
+    filesDir_r = "files/resources/"
+    filesDir_p = "files/public/"
 ;
 
 // Copy files to the project
-fs.cp(filesDir, dest, {recursive: true, force: false}, (err, data) => {
+fs.cp(filesDir_r, dest_r, {recursive: true, force: false}, (err, data) => {
+    if (err) {
+        console.error("Error Install");
+        console.error(err);
+    } else {
+        console.log("successful resource installation");
+    }
+});
+
+fs.cp(filesDir_p, dest_p, {recursive: true, force: false}, (err, data) => {
     if (err) {
         console.error("Error Install");
         console.error(err);
