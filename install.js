d0518a892fc5346c848e00b396b203ee645a63d4
John Sánchez Alvarez <esutoraiki@gmail.com>
2022-09-22 10:17:01 -0500

    Correction for install.js

--- install.js
+++ install.js
@@ -2,23 +2,12 @@ const
     fs = require("fs"),
 
     // args = process.argv.slice(2),
-    dest_r = "../../",
-    dest_p = "../../../public",
-    filesDir_r = "files/resources/"
-    filesDir_p = "files/public/"
+    dest = "../../",
+    filesDir = "files/"
 ;
 
 // Copy files to the project
 fs.cp(filesDir_r, dest_r, {recursive: true, force: false}, (err, data) => {
-    if (err) {
-        console.error("Error Install");
-        console.error(err);
-    } else {
-        console.log("successful resource installation");
-    }
-});
-
-fs.cp(filesDir_p, dest_p, {recursive: true, force: false}, (err, data) => {
     if (err) {
         console.error("Error Install");
         console.error(err);
